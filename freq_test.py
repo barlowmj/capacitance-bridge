1b61e082510ed301fa0b5a32f1345327fa5615ad
barlowmj <barlowmj@umich.edu>
2021-04-07 16:37:53 -0400

    trash

--- freq_test.py
+++ freq_test.py
@@ -10,7 +10,7 @@ def main():
         i += 1
     func_gen_loc = resources[int(input("Which device corresponsds to the function generator? "))]
     lock_in_loc = resources[int(input("Which device corresponds the the lock-in amplifier? "))]
-    fun_gen = rm.open_resource(func_gen_loc)
+    func_gen = rm.open_resource(func_gen_loc)
     lock_in = rm.open_resource(lock_in_loc)
     print("Loading default excitation...")
     func_gen.write('SOUR1:FUNC SIN')
@@ -23,9 +23,7 @@ def main():
     func_gen.write('SOUR2:VOLT +0.25')
     func_gen.write('SOUR2:VOLT:OFF 0')
     command1 = input("Turn on output [y when ready]: ")
-    if (command1 == 'y;):
-        continue
-    else:
+    if (command1 == 'y'):
         return
     print("Turning on output...")
     func_gen.write('OUTP1 1; OUTP2 1')
